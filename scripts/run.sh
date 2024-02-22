e271456a22bac3e04098cdbdd985819d5db677a4
SmallL-U <smalll-u@qq.com>
2024-02-22 23:46:33 +0800

    fix grep join

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,7 @@
 if [ "$interrupt_enable" == "true" ]; then
   info "Docker interrupt is starting..."
   # Stop all containers with the specified keywords
-  docker ps -a --format "{{.Names}}" | grep "$(printf -- "-e %s " "${interrupt_keywords[@]}")" | xargs docker stop
+  docker ps -a --format "{{.Names}}" | grep -E "$(printf "%s|" "${interrupt_keywords[@]}" | sed 's/|$//')" | xargs -r docker stop
   info "Docker interrupt is started"
 else
   info "Docker interrupt is disabled, canceled start interrupt"
